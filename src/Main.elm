f7589a347cb311437f5d8b33cd10ad3592e6a651
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-02-21 09:20:20 +0900

    barebone Browser.application

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,58 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Navigation as Nav
 import Dict exposing (update)
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, sub, text)
+import Url
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.application
         { init = init
         , update = update
         , view = view
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
         }
 
 
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
 type alias Model =
-    Int
+    { key : Nav.Key
+    , url : Url.Url
+    }
 
 
-init : Model
-init =
-    0
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model key url, Cmd.none )
 
 
 type Msg
-    = Msg
+    = UrlChanged Url.Url
+    | LinkClicked Browser.UrlRequest
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Msg ->
-            model
+        _ ->
+            ( model, Cmd.none )
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view _ =
-    div [] [ text "hello" ]
+    { title = "titleee"
+    , body =
+        [ div
+            []
+            [ text "hello" ]
+        ]
+    }

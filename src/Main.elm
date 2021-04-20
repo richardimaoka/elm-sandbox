83544a3ee14be033ed89aa3fc8d040355b88f548
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-20 21:47:32 +0900

    decode true/false

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,18 @@
 module Main exposing (main)
 
 import Dict
-import Html exposing (text)
-import Json.Decode exposing (decodeString, dict, int)
+import Html exposing (a, text)
+import Json.Decode exposing (bool, decodeString, dict, int)
 
 
 main =
-    case decodeString (dict int) "{ \"a042\": 42 }" of
+    case decodeString bool "ture" of
         Ok a ->
-            case Dict.get "a042" a of
-                Just i ->
-                    text <| String.fromInt i
+            if a then
+                text "true"
 
-                Nothing ->
-                    text "wrong key"
+            else
+                text "false"
 
         Err _ ->
             text "Err"

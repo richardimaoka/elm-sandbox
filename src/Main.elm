231b146f4741b075e2c2123b352cfb0559cd1a3d
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-20 21:36:36 +0900

    string decoder cannot decode object

--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,7 @@ import Json.Decode exposing (decodeString, string)
 
 
 main =
-    case decodeString string "\"a042\"" of
+    case decodeString string "{ \"a042\": 42 }" of
         Ok a ->
             text a
 

ccd2e8538b07d8bcb4d802cbaf1233d8a37bb5cc
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-20 21:45:00 +0900

    decoding dict

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,19 @@
 module Main exposing (main)
 
+import Dict
 import Html exposing (text)
-import Json.Decode exposing (decodeString, string)
+import Json.Decode exposing (decodeString, dict, int)
 
 
 main =
-    case decodeString string "{ \"a042\": 42 }" of
+    case decodeString (dict int) "{ \"a042\": 42 }" of
         Ok a ->
-            text a
+            case Dict.get "a042" a of
+                Just i ->
+                    text <| String.fromInt i
+
+                Nothing ->
+                    text "wrong key"
 
         Err _ ->
             text "Err"

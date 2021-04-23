bcfa9938b5b2323ebd8a48630db6747c9603895a
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-23 23:54:15 +0900

    follow the instruction

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,50 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Bulma.Classes as Bulma
+import GraphQLClient exposing (makeGraphQLQuery)
+import Graphql.Http
+import Graphql.Operation exposing (RootQuery)
+import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
+import Html exposing (Html, div, figure, h1, img, p, text)
+import Html.Attributes exposing (class, src)
+import List
+import Pokemon.Object
+import Pokemon.Object.Pokemon as Pokemon
+import Pokemon.Query as Query exposing (PokemonsRequiredArguments)
+import Pokemon.ScalarCodecs
+import RemoteData exposing (RemoteData)
 
 
 
 ---- MODEL ----
 
 
+type alias Pokemon =
+    { id : Pokemon.ScalarCodecs.Id
+    , name : Maybe String
+    , image : Maybe String
+    }
+
+
+type alias Pokemons =
+    Maybe (List (Maybe Pokemon))
+
+
+type alias PokemonData =
+    RemoteData (Graphql.Http.Error Pokemons) Pokemons
+
+
 type alias Model =
-    {}
+    { pokemons : PokemonData }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { pokemons = RemoteData.Loading
+      }
+    , fetchPokemons 151
+    )
 
 
 
@@ -23,12 +52,19 @@ init =
 
 
 type Msg
-    = NoOp
+    = FetchDataSuccess PokemonData
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        FetchDataSuccess response ->
+            updatePokemonsData response model Cmd.none
+
+
+updatePokemonsData : PokemonData -> Model -> Cmd Msg -> ( Model, Cmd Msg )
+updatePokemonsData data model cmd =
+    ( { model | pokemons = data }, cmd )
 
 
 
@@ -37,10 +73,80 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Ellm App is working!" ]
+    div [ class Bulma.container ]
+        [ img [ src "https://i.gyazo.com/480551bded5134ddacf08616b2595717.png" ] []
+        , h1 [ class Bulma.title, class Bulma.is4, class Bulma.mb6 ] [ text "Pokemons with elm-graphql" ]
+        , renderPokemonList model.pokemons
         ]
+
+
+renderPokemonList : PokemonData -> Html Msg
+renderPokemonList pokemonData =
+    let
+        renderPokemon pokemon =
+            div [ class Bulma.card ]
+                [ div [ class Bulma.cardImage ]
+                    [ figure [ class Bulma.image, class Bulma.is16by9, class Bulma.mx5, class Bulma.mt5 ]
+                        [ img [ src (pokemon.image |> Maybe.withDefault "") ] []
+                        ]
+                    ]
+                , div [ class Bulma.cardContent ]
+                    [ p [ class Bulma.isSize4 ] [ text (pokemon.name |> Maybe.withDefault "") ]
+                    ]
+                ]
+
+        renderPokemons maybePokemons =
+            maybePokemons
+                |> Maybe.withDefault []
+                |> List.map
+                    (\maybePokemon ->
+                        div [ class Bulma.column, class Bulma.is3 ]
+                            [ maybePokemon
+                                |> Maybe.map renderPokemon
+                                |> Maybe.withDefault (text "")
+                            ]
+                    )
+                |> div [ class Bulma.columns, class Bulma.isMultiline ]
+    in
+    case pokemonData of
+        RemoteData.NotAsked ->
+            p [ class Bulma.isSize4, class Bulma.hasTextCentered ] [ text "not" ]
+
+        RemoteData.Success maybePokemons ->
+            renderPokemons maybePokemons
+
+        RemoteData.Loading ->
+            p [ class Bulma.isSize4, class Bulma.hasTextCentered ] [ text "loading..." ]
+
+        RemoteData.Failure err ->
+            p [ class Bulma.isSize4, class Bulma.hasTextCentered ] [ text "Error" ]
+
+
+
+---- GraphQL API ----
+
+
+pokemonsRequiredArguments : Int -> PokemonsRequiredArguments
+pokemonsRequiredArguments num =
+    { first = num }
+
+
+pokemonListSelection : SelectionSet Pokemon Pokemon.Object.Pokemon
+pokemonListSelection =
+    SelectionSet.map3 Pokemon
+        Pokemon.id
+        Pokemon.name
+        Pokemon.image
+
+
+fetchPokemonsQuery : Int -> SelectionSet Pokemons RootQuery
+fetchPokemonsQuery num =
+    Query.pokemons (pokemonsRequiredArguments num) pokemonListSelection
+
+
+fetchPokemons : Int -> Cmd Msg
+fetchPokemons num =
+    makeGraphQLQuery (fetchPokemonsQuery num) (RemoteData.fromResult >> FetchDataSuccess)
 
 
 
